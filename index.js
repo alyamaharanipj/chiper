4d3a316cae8880357e6a1087ca16ac35f01f7821
= <alya.maharani.tif18@polban.ac.id>
2021-10-10 15:51:12 +0700

    chiper

---
+++ index.js
@@ -0,0 +1,23 @@
+const chiper = (word, num) => {
+    const alpha = [
+        'a', 'b', 'c', 'd', 'e',
+        'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+
+    let newWord = ''
+    for(let i = 0; i < word.length; i++){
+        if(word[i].match(/^[A-Z]+$/i)){
+            let index = alpha.findIndex( el => el === word[i].toLowerCase())
+            let newIndex = index + num
+            if(newIndex >= 26){
+                newWord += alpha[newIndex - 26]
+            } else newWord += alpha[index + num]
+        } else newWord += word[i]
+    }
+    console.log(newWord)
+}
+
+chiper('hello worldz',3)
